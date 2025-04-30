b2e8afb8c5b0fc99ddd596f81e5db50ca4a555bd
ThePigeonNotes <houseofel023@gmail.com>
2025-04-30 23:04:40 +0800

    Add files via upload

---
+++ card.js
@@ -0,0 +1,20 @@
+function showSurprise(){
+    const gif = document.getElementById("gif");
+    gif.style.display = "block";
+
+    document.getElementById("main-title").innerText = "You are the best mom ever!";
+    document.getElementById("subtext").innerText = "💖THANK YOU FOR EVERYTHING!💖";
+
+    for(let i=0; i<20; i++){
+        const flower = document.createElement("div");
+        flower.className = "flower";
+        flower.innerText="🌸";
+        flower.style.left = Math.random() * window.innerWidth + "px";
+        flower.style.animationDuration = (Math.random()*2+2)+"s";
+        document.body.appendChild(flower);
+
+        setTimeout(()=> {
+            flower.remove();
+        },3000);
+    }
+}
